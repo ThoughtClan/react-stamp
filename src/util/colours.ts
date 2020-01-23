09b6fdf9badd94663d477b2a36eb5444c86dcef8
naoey <naoey@protonmail.ch>
2020-01-23 14:46:30 +0530

    Add colour and flex helpers

---
+++ src/util/colours.ts
@@ -0,0 +1,12 @@
+enum Colours {
+  Black = '#000000',
+  Grey1 = '# 666666',
+  Grey2 = '#999999',
+  Grey3 = '# CCCCCC',
+  Grey4 = '#D9D9D9',
+  Grey5 = '#F1F1F1',
+  White = '#FFFFFF',
+  Transparent = 'rgba(0, 0, 0, 0)',
+}
+
+export default Colours;
