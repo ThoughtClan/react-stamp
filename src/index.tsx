34d555d4954c2abd8c07113a0b539cd52b682fd8
naoey <naoey@protonmail.ch>
2020-01-23 20:34:39 +0530

    Update name in licenses and package

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2020 naoey
+ * Copyright (c) 2020 ThoughtClan
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
