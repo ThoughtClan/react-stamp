fd1dd4429357ac36f9793a6ba825f98dcf3bee4a
naoey <naoey@protonmail.ch>
2020-01-24 09:31:45 +0530

    Initial tests setup

--- test/components/StampCreator.test.ts
+++
@@ -1,7 +0,0 @@
-import StampCreator from '../../src/components/StampCreator';
-
-describe('StampCreator', () => {
-  it('is truthy', () => {
-    expect(StampCreator).toBeTruthy()
-  })
-})
---
+++ tests/.setup.js
@@ -0,0 +1,20 @@
+/**
+ * Defines the React 16 Adapter for Enzyme.
+ *
+ * @link http://airbnb.io/enzyme/docs/installation/#working-with-react-16
+ * @copyright 2017 Airbnb, Inc.
+ */
+const enzyme = require("enzyme");
+const Adapter = require("enzyme-adapter-react-16");
+
+enzyme.configure({ adapter: new Adapter() });
+
+/**
+ * Get rids of the missing requestAnimationFrame polyfill warning.
+ *
+ * @link https://reactjs.org/docs/javascript-environment-requirements.html
+ * @copyright 2004-present Facebook. All Rights Reserved.
+ */
+global.requestAnimationFrame = function (callback) {
+  setTimeout(callback, 0);
+};
